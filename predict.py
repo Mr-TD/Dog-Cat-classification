d6d1c09948b0981d25f7ccc390e26d8dbff04739
Dharmik Trivedi <57656849+Mr-TD@users.noreply.github.com>
2020-08-19 11:50:05 +0000

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 18:45:05 2020
+
+@author: sudhanshukumar
+"""
+
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+from PIL import Image
+
+class dogcat:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    def predictiondogcat(self):
+        # load model
+        model = load_model('https://drive.google.com/file/d/1PiVp82eCLYNHiExyThK7fPwsDQ26BI39/view?usp=sharing')
+
+        # summarize model
+        #model.summary()
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (256, 256))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = model.predict(test_image)
+
+        if result[0][0] == 1:
+            prediction = ' This Image is of dog '
+            return [prediction]
+        else:
+            prediction = ' This Image is of cat '
+            return [prediction]
+
+
